9760f44917b5725fc9b039235077ff7cc8880973
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-06-29 14:33:17 -0300

    Configurando Heroku

--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,12 @@ module.exports = {
   url: process.env.DATABASE_URL,
   migrations: ["dist/database/migrations/*.js"],
   entities: ["dist/entities/*.js"],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   cli: {
     migrationsDir: "src/database/migrations",
     entitiesDir: "src/entities",
